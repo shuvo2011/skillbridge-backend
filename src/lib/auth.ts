3c1936b62d171ad5ed93a3c15ce21a5719e2851f
arifuddincoder <shuvombstu2011@gmail.com>
2026-04-02 17:00:23 +0600

    add base url in auth.ts file

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -14,6 +14,7 @@ const transporter = nodemailer.createTransport({
 });
 
 export const auth = betterAuth({
+	baseURL: process.env.BETTER_AUTH_URL || "http://localhost:5050",
 	database: prismaAdapter(prisma, {
 		provider: "postgresql",
 	}),
